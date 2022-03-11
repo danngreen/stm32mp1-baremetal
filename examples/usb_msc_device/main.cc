06423b087797fe4e231c9886db43662a56be074f
Dan Green <danngreen1@gmail.com>
2022-03-10 17:31:22 -0800

    Enabling IRQ in main

--- examples/usb_msc_device/main.cc
+++ examples/usb_msc_device/main.cc
@@ -1,5 +1,7 @@
+#include "drivers/interrupt.hh"
 #include "drivers/leds.hh"
 #include "drivers/uart.hh"
+#include "irq_ctrl.h"
 #include "stm32mp1xx.h"
 #include "system_clk.hh"
 #include "usbd_core.h"
@@ -12,8 +14,11 @@
 // using namespace OSD32BRK;
 
 // Uncomment these to use the STM32MP1Disco board:
-#include "stm32disco_conf.hh"
-using namespace STM32MP1Disco;
+// #include "stm32disco_conf.hh"
+// using namespace STM32MP1Disco;
+
+using DebugPin0 = Led<GPIOG_BASE, 9, LedActive::High>;
+using DebugPin1 = Led<GPIOA_BASE, 11, LedActive::High>;
 
 extern PCD_HandleTypeDef hpcd;
 void main()
@@ -23,20 +28,42 @@ void main()
 	uart.write("Connect a USB cable to a computer\r\n");
 	uart.write("You should see a 128MB unformatted drive appear.\r\n");
 
-	GreenLED green1;
+	DebugPin0 green1;
+	DebugPin1 green2;
 
+	green1.on();
 	green1.off();
+	green1.on();
+	green1.off();
+
+	green2.on();
+	green2.off();
+	green2.on();
+	green2.off();
 
 	SystemClocks::init();
 
 	USBD_HandleTypeDef USBD_Device;
 
+	usbd_descriptors_initialize(1);
 	auto init_ok = USBD_InitComposite(&USBD_Device);
 	if (init_ok != USBD_OK) {
 		uart.write("USB Device failed to initialize!\r\n");
 		uart.write("Error code: ");
 		uart.write(static_cast<int>(init_ok));
 	}
+
+	// InterruptManager::registerISR(OTG_IRQn, std::bind_front(HAL_PCD_IRQHandler, &hpcd));
+	InterruptManager::registerISR(OTG_IRQn, [&green2] {
+		green2.on();
+		HAL_PCD_IRQHandler(&hpcd);
+		green2.off();
+	});
+	GIC_DisableIRQ(OTG_IRQn);
+	GIC_SetTarget(OTG_IRQn, 1);
+	GIC_SetPriority(OTG_IRQn, 0b01111000);
+	GIC_SetConfiguration(OTG_IRQn, 0b10); // Edge triggered
+	GIC_EnableIRQ(OTG_IRQn);
 
 	USBD_AddClass(&USBD_Device, USBD_MSC_CLASS);
 
@@ -49,8 +76,8 @@ void main()
 	bool led_state = false;
 	while (1) {
 
-		// Check for new USB events
-		HAL_PCD_IRQHandler(&hpcd);
+		// // Check for new USB events
+		// HAL_PCD_IRQHandler(&hpcd);
 
 		uint32_t tm = HAL_GetTick();
 		if (tm > (last_tm + 500)) {
@@ -63,9 +90,4 @@ void main()
 			led_state = !led_state;
 		}
 	}
-}
-
-extern "C" void IRQ_Initialize(void)
-{
-	// do nothing, just a stub
 }
