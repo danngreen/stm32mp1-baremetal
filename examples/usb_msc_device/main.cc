d1b004cec081a24bacc92e0a9694150646e0097f
Dan Green <danngreen1@gmail.com>
2022-03-12 15:40:35 -0800

    main starts up same, uses OSD32 board

--- examples/usb_msc_device/main.cc
+++ examples/usb_msc_device/main.cc
@@ -1,24 +1,26 @@
-#include "drivers/interrupt.hh"
+//#include "drivers/interrupt.hh"
+#include "hardware.h"
+
 #include "drivers/leds.hh"
 #include "drivers/uart.hh"
 #include "irq_ctrl.h"
 #include "stm32mp1xx.h"
-#include "system_clk.hh"
+//#include "system_clk.hh"
 #include "usbd_core.h"
 #include "usbd_desc.h"
 #include "usbd_msc_storage.h"
 #include <cstdint>
 
 // Uncomment these to use the OSD32BRK board:
-//#include "osd32brk_conf.hh"
-// using namespace OSD32BRK;
+#include "osd32brk_conf.hh"
+using namespace OSD32BRK;
 
 // Uncomment these to use the STM32MP1Disco board:
 // #include "stm32disco_conf.hh"
 // using namespace STM32MP1Disco;
 
-using DebugPin0 = Led<GPIOG_BASE, 9, LedActive::High>;
-using DebugPin1 = Led<GPIOA_BASE, 11, LedActive::High>;
+// using GreenLED = Led<GPIOG_BASE, 9, LedActive::High>;
+// using GreenLED2 = Led<GPIOA_BASE, 11, LedActive::High>;
 
 extern PCD_HandleTypeDef hpcd_USB_OTG;
 void main()
@@ -28,8 +30,8 @@ void main()
 	uart.write("Connect a USB cable to a computer\r\n");
 	uart.write("You should see a 128MB unformatted drive appear.\r\n");
 
-	DebugPin0 green1;
-	DebugPin1 green2;
+	GreenLED green1;
+	GreenLED2 green2;
 
 	green1.on();
 	green1.off();
@@ -41,11 +43,17 @@ void main()
 	green2.on();
 	green2.off();
 
-	SystemClocks::init();
+	// SystemClocks::init();
 
 	USBD_HandleTypeDef USBD_Device;
 
+	__disable_irq();
+	cpu_initialize(); // в случае ARM - инициализация прерываний и контроллеров, AVR - запрет JTAG
+	hardware_timer_initialize(TICKS_FREQUENCY);
+
+	// board_usb_initialize():
 	usbd_descriptors_initialize(1);
+
 	auto init_ok = USBD_InitComposite(&USBD_Device);
 	if (init_ok != USBD_OK) {
 		uart.write("USB Device failed to initialize!\r\n");
@@ -54,23 +62,23 @@ void main()
 	}
 
 	// InterruptManager::registerISR(OTG_IRQn, std::bind_front(HAL_PCD_IRQHandler, &hpcd));
-	InterruptManager::registerISR(OTG_IRQn, [&green2] {
-		green2.on();
-		HAL_PCD_IRQHandler(&hpcd_USB_OTG);
-		green2.off();
-	});
-	GIC_DisableIRQ(OTG_IRQn);
-	GIC_SetTarget(OTG_IRQn, 1);
-	GIC_SetPriority(OTG_IRQn, 0b01111000);
-	GIC_SetConfiguration(OTG_IRQn, 0b10); // Edge triggered
+	// InterruptManager::registerISR(OTG_IRQn, [&green2] {
+	// 	green2.on();
+	// 	HAL_PCD_IRQHandler(&hpcd_USB_OTG);
+	// 	green2.off();
+	// });
+	// GIC_DisableIRQ(OTG_IRQn);
+	// GIC_SetTarget(OTG_IRQn, 1);
+	// GIC_SetPriority(OTG_IRQn, 0b01111000);
+	// GIC_SetConfiguration(OTG_IRQn, 0b10); // Edge triggered
 
 	USBD_AddClass(&USBD_Device, USBD_MSC_CLASS);
+	// USBD_MSC_RegisterStorage(&USBD_Device, &USBD_MSC_fops);
 
-	USBD_MSC_RegisterStorage(&USBD_Device, &USBD_MSC_fops);
+	// GIC_EnableIRQ(OTG_IRQn);
 
-	USBD_Start(&USBD_Device);
-
-	GIC_EnableIRQ(OTG_IRQn);
+	__enable_irq();
+	USBD_Start(&USBD_Device); // board_usb_activate()
 
 	// Blink green1 light at 1Hz
 	uint32_t last_tm = 0;
